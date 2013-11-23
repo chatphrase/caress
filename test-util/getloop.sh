2a1d943c41eb6e953c421159cf9cf292dabe3430
Stuart P. Bentley <stuart@testtrack4.com>
2013-11-23 18:57:50 -0500

    Fix crazy sed in getloop.sh

--- test-util/getloop.sh
+++ test-util/getloop.sh
@@ -1,7 +1,7 @@
 trap "exit" int
 
 while true; do
-  curl -v ${etag:+-H "If-None-Match: $etag"} localhost:3000$1 -D >(sed -rn 's/^Etag:/{s/Etag:\s*//p}' | read etag)
+  curl -v ${etag:+-H "If-None-Match: $etag"} localhost:3000$1 -D >(sed -rn 's/^Etag:\s*//p' | read etag)
   if [ $? == 0 ]; then
     sleep ${2-0}
   else
