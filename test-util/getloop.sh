5612c8b52c3a05848edfaa6d398f79cc8a517f2c
Stuart P. Bentley <stuart@testtrack4.com>
2013-11-23 19:29:24 -0500

    Wuss out and use a temp file for the etag

--- test-util/getloop.sh
+++ test-util/getloop.sh
@@ -1,8 +1,11 @@
 trap "exit" int
 
+etag=`mktemp`
+
 while true; do
-  curl -v ${etag:+-H "If-None-Match: $etag"} localhost:3000$1 -D >(sed -rn 's/^Etag:\s*//p' > >(read etag))
+  curl -v ${inm:+-H "If-None-Match: $inm"} localhost:3000$1 -D >(sed -rn 's/^Etag:\s*//p' > $etag)
   if [ $? == 0 ]; then
+    inm=`cat $etag`
     sleep ${2-0}
   else
     exit
