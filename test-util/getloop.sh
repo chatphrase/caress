923904c0636561f30fec04a2b23cc99fe5869493
Stuart P. Bentley <stuart@testtrack4.com>
2013-11-23 19:42:34 -0500

    Don't lose the If-None-Match when the server doesn't retun an Etag

--- test-util/getloop.sh
+++ test-util/getloop.sh
@@ -5,7 +5,8 @@ etag=`mktemp`
 while true; do
   curl -v ${inm:+-H "If-None-Match: $inm"} localhost:3000$1 -D >(sed -rn 's/^Etag:\s*//p' > $etag)
   if [ $? == 0 ]; then
-    inm=`cat $etag`
+    pinm=`cat $etag`
+    inm=${pinm:-$inm}
     sleep ${2-0}
   else
     exit
