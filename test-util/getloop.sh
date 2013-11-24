f8f23706f19ed44676ab54711fcefcec473b56fd
Stuart P. Bentley <stuart@testtrack4.com>
2013-11-23 19:51:03 -0500

    Make getloop.sh fail on 404

--- test-util/getloop.sh
+++ test-util/getloop.sh
@@ -3,7 +3,7 @@ trap "exit" int
 etag=`mktemp`
 
 while true; do
-  curl -v ${inm:+-H "If-None-Match: $inm"} localhost:3000$1 -D >(sed -rn 's/^Etag:\s*//p' > $etag)
+  curl -v -f ${inm:+-H "If-None-Match: $inm"} localhost:3000$1 -D >(sed -rn 's/^Etag:\s*//p' > $etag)
   if [ $? == 0 ]; then
     pinm=`cat $etag`
     inm=${pinm:-$inm}
